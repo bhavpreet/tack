c8ed76771daa96e9fef1a8d14e068addaaae2e42
bhavpreet <bhavpreet@gmail.com>
2017-03-21 16:03:01 +0530

    adding mongodb module

---
+++ modules/mongodb/io.tf
@@ -0,0 +1,18 @@
+variable "depends-id" {}
+variable "instance-type" {}
+variable "key-name" {}
+variable "name" {}
+variable "region" {}
+variable "security-group-id" {}
+variable "default-group-id" {}
+variable "subnet-ids" {}
+variable "volume_size" {
+  default = {
+    ebs = 250
+    root = 52
+  }
+}
+variable "vpc-id" {}
+
+output "depends-id" { value = "${ null_resource.dummy_dependency.id }" }
+output "mongodb-ip" { value = "${ aws_instance.mongodb.private_ip }" }
