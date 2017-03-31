c8ec4f2fdb30de1cee607165a4cf3ac8b1f355e8
bhavpreet <bhavpreet@mojhi.com>
2017-03-31 10:57:29 +0000

    adding support for mysql

---
+++ modules/mysql/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "mysql" {
+  ami           = "${ var.ami-id }"
+  instance_type = "${ var.instance-type }"
+  key_name = "${ var.key-name }"
+  associate_public_ip_address = true
+
+  source_dest_check = true
+  subnet_id = "${ element( split(",", var.subnet-ids), 0 ) }"
+  
+  # Storage
+  root_block_device {
+    volume_size = "${ var.volume_size["root"] }"
+    volume_type = "gp2"
+  }
+
+  vpc_security_group_ids = [
+      "${ var.security-group-id }",
+      "${ var.default-group-id }"
+  ]
+  
+  tags {
+    builtWith = "terraform"
+    depends-id = "${ var.depends-id }"
+    kz8s = "${ var.name }"
+    Name = "kz8s-${ var.name }-mysql"
+    role = "mysql"
+    visibility = "private"
+  }
+}
+
+resource "null_resource" "dummy_dependency" {
+  depends_on = [ "aws_instance.mysql" ]
+}
---
+++ modules/mysql/io.tf
@@ -0,0 +1,19 @@
+variable "ami-id" {}
+variable "depends-id" {}
+variable "instance-type" {}
+variable "key-name" {}
+variable "name" {}
+variable "region" {}
+variable "security-group-id" {}
+variable "default-group-id" {}
+variable "subnet-ids" {}
+variable "volume_size" {
+  default = {
+    ebs = 250
+    root = 52
+  }
+}
+variable "vpc-id" {}
+
+output "depends-id" { value = "${ null_resource.dummy_dependency.id }" }
+output "mysql-ip" { value = "${ aws_instance.mysql.private_ip }" }
